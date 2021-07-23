9325f69cd4545840737845260921b1c4d36637b0
zb1990 <>
2021-07-23 20:34:18 +0000

    clean app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <template>
   <div class="text-gray-700">
-    <zb-dns-form classes="px-2">
-      <zb-input-text label="Hostname"/>
-      <zb-select label="Domain" :options="state.domains"/>
-    </zb-dns-form>
+    <index-page/>
   </div>
 </template>
 
 <script setup>
-  import { reactive } from 'vue'
-  import ZbDnsForm from './components/Form.vue'
-  import ZbInputText from './components/InputText.vue'
-  import ZbSelect from './components/Select.vue'
-
-  const state = reactive({
-    domains: [
-      '.google.com',
-      '.facebook.com'
-    ]
-  })
-
+  import IndexPage from './pages/index.vue'
 </script>
---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-gray-700">
+    <zb-dns-form classes="px-2">
+      <zb-input-text label="Hostname"/>
+      <zb-select label="Domain" :options="state.domains"/>
+    </zb-dns-form>
+  </div>
+</template>
+
+<script setup>
+  import { reactive } from 'vue'
+  import ZbDnsForm from '../components/Form.vue'
+  import ZbInputText from '../components/InputText.vue'
+  import ZbSelect from '../components/Select.vue'
+
+  const state = reactive({
+    domains: [
+      '.google.com',
+      '.facebook.com'
+    ]
+  })
+
+</script>
